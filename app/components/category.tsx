5fe21e8b9b286694f621625863ee209f7ddb0ca3
mfaiz2003 <mfaiz76542@gmail.com>
2025-12-19 20:13:26 +0530

    Make components type-safe with TypeScript props

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -1,4 +1,8 @@
-export default function Category({ className }) {
+type CategoryProps = {
+  className?: string; // optional string
+};
+
+export default function Category({ className }: CategoryProps) {
   return (
     <div className={`flex gap-6 mt-10 text-gray-500 ${className}`}>
       <button className="px-10 py-3 bg-black border-2 border-gray-300">Footwear</button>
--- app/components/gender.tsx
+++ app/components/gender.tsx
@@ -1,6 +1,10 @@
-export default function Gender({ className }) {
+type GenderProps = {
+  className?: string; // optional string
+};
+
+export default function Gender({ className }: GenderProps) {
   return (
-    <div className="gap-6 mt-10 text-gray-500">
+    <div className={`gap-6 mt-10 text-gray-500 ${className}`}>
       <button className="px-10 py-3 bg-black border-2 border-gray-300">Men</button>
       <button className="px-10 py-3 border-2 border-gray-300">Women</button>
       <button className="px-10 py-3 border-2 border-gray-300">Boy</button>
