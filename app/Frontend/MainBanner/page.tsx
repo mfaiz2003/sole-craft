ade27a6db07988b4176592ede73d3da6db417b90
mfaiz2003 <mfaiz76542@gmail.com>
2025-12-22 12:50:22 +0530

    initial changes

--- app/Frontend/MainBanner/page.tsx
+++ app/Frontend/MainBanner/page.tsx
@@ -28,34 +28,34 @@ export default function MainBanner(){
 
 
     return(
-    <>    
-    <Snowfall color="black" />
+    <>  
 
     <div className="">
       <Header/>
     </div>   
     
-<section className="container h-screen">
-  <div className="flex flex-col md:grid md:grid-cols-2 h-full">
-     <div className="relative h-1/2 md:h-screen overflow-hidden">
-      <Image
-        src={images[index]}
-        alt="Banner Image"
-        fill
-        unoptimized
-        className="object-cover"
-      />
-    </div>
-    <div className="md:px-28 px-6 h-1/2 md:h-auto flex flex-col justify-center">
-      {Data.BannerItem.description.map((line, index) => (
-        <h3 key={index} className="md:text-8xl text-6xl font-bold">{line}</h3>
-      ))}
-      {Data.BannerItem.subdescription.map((line, index) => (
-        <p key={index} className="md:text-xl text-gray-700">{line}</p>
-      ))}
-    </div>
-  </div>
-</section>
+      <section className="container h-screen">
+        <div className="flex flex-col md:grid md:grid-cols-2 h-full">
+           <div className="relative h-1/2 md:h-screen overflow-hidden">
+            <Image
+              src={images[index]}
+              alt="Banner Image"
+              fill
+              unoptimized
+              className="object-cover"
+            />
+          </div>
+          <div className="md:px-28 px-6 h-1/2 md:h-auto flex flex-col justify-center">
+          <Snowfall color="black" />
+            {Data.BannerItem.description.map((line, index) => (
+              <h3 key={index} className="md:text-8xl text-6xl font-bold">{line}</h3>
+            ))}
+            {Data.BannerItem.subdescription.map((line, index) => (
+              <p key={index} className="md:text-xl text-gray-700">{line}</p>
+            ))}
+          </div>
+        </div>
+      </section>
 
 
       {/* support & Brand section */}
