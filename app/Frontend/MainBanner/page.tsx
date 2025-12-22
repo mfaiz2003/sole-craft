6c8c30c30e135a06f7bf5679b0c77c3422574f67
mfaiz2003 <mfaiz76542@gmail.com>
2025-12-22 12:34:47 +0530

    brand logo size decrease

--- app/Frontend/MainBanner/page.tsx
+++ app/Frontend/MainBanner/page.tsx
@@ -64,13 +64,13 @@ export default function MainBanner(){
     
        <div className="grid md:grid-cols-6 grid-cols-3 gap-10 md:px-28 px-6 md:py-0 py-3 bg-black">
           {Data.BrandItem.map((items) => (
-            <div key={items.id} className="flex justify-center items-center">
+            <div key={items.id} className="flex justify-center items-center ">
               <Image
                 src={items.image}
                 alt="brand-card"
                 width={90}
                 height={60}
-                className="object-contain"
+                className="object-contain md:h-0 h-10"
               />
             </div>
           ))}
@@ -78,12 +78,13 @@ export default function MainBanner(){
         <div className="text-center font-bold md:text-5xl text-3xl md:mt-10 mt-5">
           <h3>Shop By Brands</h3>
         </div>
+
         <div className="grid md:grid-cols-[40%_60%] md:px-28 px-4 md:pt-15 pt-5">
-        <div className="pt-5 pb-5 w-auto">
+        <div className="py-5 w-auto">
           <h5 className="text-2xl text-gray-700">Our Trending shoe</h5>
           <h3 className=" font-bold md:text-6xl text-5xl ">Most Popular<br /> Products</h3>
           <p className="text-xl text-gray-700">Top trending products everyone’s buying</p>
-          <Button className="mt-10 text-white"/>
+          <Button className="md:mt-10 mt-5 text-white"/>
         </div>
         <div className="grid md:grid-cols-3 grid-cols-2 gap-4 w-full">
           {Data.TrendingItem.map((items) => (
